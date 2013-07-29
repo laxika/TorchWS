dc54d46ce28d747e31990ed439b81c6a7b9eb940
laxika <laxika91@gmail.com>
2013-07-29 21:19:13 +0200

    Updated to netty 4.0.4

--- src/torch/ServerHandler.java
+++ src/torch/ServerHandler.java
@@ -3,7 +3,7 @@ package torch;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundMessageHandlerAdapter;
+import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.FullHttpResponse;
 import io.netty.handler.codec.http.HttpHeaders;
@@ -24,20 +24,24 @@ import torch.route.RouteManager;
 import torch.session.Session;
 import torch.session.SessionManager;
 
-public class ServerHandler extends ChannelInboundMessageHandlerAdapter<Object> {
+public class ServerHandler extends SimpleChannelInboundHandler<Object> {
 
     private final RouteManager routes;
     protected static SessionManager sessionManager = new SessionManager();
 
     public ServerHandler(RouteManager container) {
-        this.routes = container;
+        routes = container;
     }
 
     @Override
-    public void messageReceived(ChannelHandlerContext ctx, Object msg) throws Exception {
+    public void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
         if (msg instanceof HttpRequest) {
             HttpRequest request = (HttpRequest) msg;
             
+            if (HttpHeaders.is100ContinueExpected(request)) {
+                send100Continue(ctx);
+            }
+
             Route target = routes.calculateRouteByUrl(request.getUri(), RequestMethod.getMethodByNettyMethod(request.getMethod()));
 
             CookieStorage cookieStorage = new CookieStorage(request.headers().get(COOKIE));
@@ -45,11 +49,11 @@ public class ServerHandler extends ChannelInboundMessageHandlerAdapter<Object> {
             //Handle the message
             TorchHttpResponse response = new TorchHttpResponse(cookieStorage);
             TorchHttpRequest torchreq = new TorchHttpRequest(request, target);
-            
+
             Session session = sessionManager.getSession(cookieStorage.getCookie("SESSID").getValue());
-            
+
             //New session
-            if(session == null) {
+            if (session == null) {
                 session = sessionManager.startNewSession();
                 response.getNewCookieData().addCookie(new Cookie("SESSID", session.getSessionId()));
             }
@@ -61,42 +65,30 @@ public class ServerHandler extends ChannelInboundMessageHandlerAdapter<Object> {
                 response.setStatus(HttpResponseStatus.NOT_FOUND);
             }
 
-            writeResponse(ctx, response, HttpHeaders.isKeepAlive(request));
-        }
-    }
+            FullHttpResponse fullresponse = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, response.getStatus(), Unpooled.copiedBuffer(response.getContent(), CharsetUtil.UTF_8));
 
-    private void writeResponse(ChannelHandlerContext ctx, TorchHttpResponse response, boolean keepAlive) {
-        FullHttpResponse fullresponse = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, response.getStatus(), Unpooled.copiedBuffer(response.getContent(), CharsetUtil.UTF_8));
+            fullresponse.headers().set(CONTENT_TYPE, response.getContentType());
 
-        fullresponse.headers().set(CONTENT_TYPE, response.getContentType());
+            //Setting the new cookies
+            for (Object pairs : response.getNewCookieData()) {
+                Cookie obj = ((Map.Entry<String, Cookie>) pairs).getValue();
 
-        if (keepAlive) {
-            // Add 'Content-Length' header only for a keep-alive connection.
-            fullresponse.headers().set(CONTENT_LENGTH, fullresponse.content().readableBytes());
-            // Add keep alive header as per:
-            // - http://www.w3.org/Protocols/HTTP/1.1/draft-ietf-http-v11-spec-01.html#Connection
-            fullresponse.headers().set(CONNECTION, HttpHeaders.Values.KEEP_ALIVE);
-        }
+                fullresponse.headers().add(SET_COOKIE, ServerCookieEncoder.encode(obj.getName(), obj.getValue()));
+            }
 
-        //Setting the new cookies
-        for (Object pairs : response.getNewCookieData()) {
-            Cookie obj = ((Map.Entry<String, Cookie>) pairs).getValue();
-            
-            fullresponse.headers().add(SET_COOKIE, ServerCookieEncoder.encode(obj.getName(), obj.getValue()));
-        }
-
-        // Write the response.
-        ctx.nextOutboundMessageBuffer().add(fullresponse);
-
-        // Close the non-keep-alive connection after the write operation is done.
-        if (!keepAlive) {
-            ctx.flush().addListener(ChannelFutureListener.CLOSE);
+            // Write the response.
+            ctx.write(fullresponse);
         }
     }
 
     @Override
-    public void endMessageReceived(ChannelHandlerContext ctx) throws Exception {
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
         ctx.flush();
+    }
+
+    private static void send100Continue(ChannelHandlerContext ctx) {
+        FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.CONTINUE);
+        ctx.write(response);
     }
 
     @Override
