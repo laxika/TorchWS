9a317b704e2037b3074da176130d3be8d5287dbc
laxika <laxika91@gmail.com>
2013-07-31 22:14:30 +0200

    Fixed the routing tests.

--- test/torch/route/RouteManagerTest.java
+++ test/torch/route/RouteManagerTest.java
@@ -5,7 +5,6 @@ import org.testng.annotations.Test;
 import torch.example.routing.HelloWorldExactRoute;
 import torch.example.routing.HelloWorldWithOneVar;
 import torch.example.routing.HelloWorldWithTwoVar;
-import torch.handler.WebPage;
 import torch.http.RequestMethod;
 
 public class RouteManagerTest {
@@ -19,41 +18,33 @@ public class RouteManagerTest {
     
     @Test
     public void testGetRouteTargetWithDynamicVariables() {
-        WebPage page = new HelloWorldWithTwoVar();
+        testTarget.defineRoute("/hello/@variable/@variable2/", HelloWorldWithTwoVar.class);
         
-        testTarget.defineRoute("/hello/@variable/@variable2/", page);
-        
-        assert testTarget.calculateRouteByUrl("/hello/var1/var2", RequestMethod.GET).getTarget().equals(page) : "Dynamic variable test 1";
+        assert testTarget.calculateRouteByUrl("/hello/var1/var2", RequestMethod.GET).getTarget().equals(HelloWorldWithTwoVar.class) : "Dynamic variable test 1";
         assert testTarget.calculateRouteByUrl("/hello/lol", RequestMethod.GET) == null : "Dynamic variable test 2";
         assert testTarget.calculateRouteByUrl("/var1/var2/var3", RequestMethod.GET) == null : "Dynamic variable test 3";
     }
     
     @Test
     public void testGetRouteTargetPriority() {
-        WebPage page1 = new HelloWorldWithTwoVar();
-        WebPage page2 = new HelloWorldWithOneVar();
-        WebPage page3 = new HelloWorldExactRoute();
+        testTarget.defineRoute("/", HelloWorldWithTwoVar.class);
+        testTarget.defineRoute("/hello/@variable/@variable2/", HelloWorldWithTwoVar.class);
+        testTarget.defineRoute("/hello/@variable/lol/", HelloWorldWithOneVar.class);
+        testTarget.defineRoute("/hello/exact/route", HelloWorldExactRoute.class);
+        testTarget.defineRoute("/hello/some/post/route", HelloWorldExactRoute.class, RequestMethod.POST);
         
-        testTarget.defineRoute("/", page1);
-        testTarget.defineRoute("/hello/@variable/@variable2/", page1);
-        testTarget.defineRoute("/hello/@variable/lol/", page2);
-        testTarget.defineRoute("/hello/exact/route", page3);
-        testTarget.defineRoute("/hello/some/post/route", page3, RequestMethod.POST);
-        
-        assert testTarget.calculateRouteByUrl("/", RequestMethod.GET).getTarget().equals(page1) : "Priority test 0";
-        assert testTarget.calculateRouteByUrl("/hello/exact/route", RequestMethod.GET).getTarget().equals(page3) : "Priority test 1";
-        assert testTarget.calculateRouteByUrl("/hello/var1/var2/", RequestMethod.GET).getTarget().equals(page1) : "Priority test 2";
-        assert testTarget.calculateRouteByUrl("/hello/var1/lol/", RequestMethod.GET).getTarget().equals(page2) : "Priority test 3";
+        assert testTarget.calculateRouteByUrl("/", RequestMethod.GET).getTarget().equals(HelloWorldWithTwoVar.class) : "Priority test 0";
+        assert testTarget.calculateRouteByUrl("/hello/exact/route", RequestMethod.GET).getTarget().equals(HelloWorldExactRoute.class) : "Priority test 1";
+        assert testTarget.calculateRouteByUrl("/hello/var1/var2/", RequestMethod.GET).getTarget().equals(HelloWorldWithTwoVar.class) : "Priority test 2";
+        assert testTarget.calculateRouteByUrl("/hello/var1/lol/", RequestMethod.GET).getTarget().equals(HelloWorldWithOneVar.class) : "Priority test 3";
         assert testTarget.calculateRouteByUrl("/hello/some/post/route", RequestMethod.GET) == null : "Priority test 4";
-        assert testTarget.calculateRouteByUrl("/hello/some/post/route", RequestMethod.POST).getTarget().equals(page3) : "Priority test 5";
+        assert testTarget.calculateRouteByUrl("/hello/some/post/route", RequestMethod.POST).getTarget().equals(HelloWorldExactRoute.class) : "Priority test 5";
     }
     
     @Test
     public void testGetRouteStartPage() {
-        WebPage page = new HelloWorldWithTwoVar();
+        testTarget.defineRoute("/", HelloWorldWithTwoVar.class);
         
-        testTarget.defineRoute("/", page);
-        
-        assert testTarget.calculateRouteByUrl("/", RequestMethod.GET).getTarget().equals(page) : "Priority test 1";
+        assert testTarget.calculateRouteByUrl("/", RequestMethod.GET).getTarget().equals(HelloWorldWithTwoVar.class) : "Priority test 1";
     }
 }
