8663259b867e66b21dbed39bbbdad844b9001854
laxika <laxika91@gmail.com>
2013-07-31 22:28:00 +0200

    Working on file handling.

--- src/torch/ServerInitializer.java
+++ src/torch/ServerInitializer.java
@@ -7,6 +7,7 @@ import io.netty.handler.codec.http.HttpContentCompressor;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpRequestDecoder;
 import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.stream.ChunkedWriteHandler;
 import torch.route.RouteManager;
 
 public class ServerInitializer extends ChannelInitializer<SocketChannel> {
@@ -28,9 +29,10 @@ public class ServerInitializer extends ChannelInitializer<SocketChannel> {
         //p.addLast("ssl", new SslHandler(engine));
 
         pipeline.addLast("decoder", new HttpRequestDecoder());
-        pipeline.addLast("aggregator", new HttpObjectAggregator(1048576));
-        pipeline.addLast("encoder", new HttpResponseEncoder());
-        pipeline.addLast("deflater", new HttpContentCompressor());
+        pipeline.addLast("aggregator", new HttpObjectAggregator(1048576)); //Aggregating the request chunks to one big request
+        pipeline.addLast("encoder", new HttpResponseEncoder()); 
+        pipeline.addLast("streamer", new ChunkedWriteHandler()); //Used at file downloads
+        pipeline.addLast("deflater", new HttpContentCompressor()); //Gzip the output
         pipeline.addLast("handler", new ServerHandler(container));
     }
 }
