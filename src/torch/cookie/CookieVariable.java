038bc4cb40a922f74f52ed35818afd210bdff88c
laxika <laxika91@gmail.com>
2013-08-07 21:08:25 +0200

    Cleanup the CookieVariable class.

--- src/torch/cookie/CookieVariable.java
+++ src/torch/cookie/CookieVariable.java
@@ -1,47 +1,20 @@
 package torch.cookie;
 
 public class CookieVariable {
+
     private final String name;
-    private String value;
-    private boolean fresh = false;
-    
-    public CookieVariable(String name, String value) {
-        this.name = name;
-        this.value = value;
-    }
-    
-    public CookieVariable(String name, String value, boolean fresh) {
-        this.name = name;
-        this.value = value;
-        this.fresh = fresh;
+    private final String value;
+
+    public CookieVariable(String cookieName, String cookieValue) {
+        this.name = cookieName;
+        this.value = cookieValue;
     }
 
-    /**
-     * @return the name
-     */
     public String getName() {
         return name;
     }
 
-    /**
-     * @return the value
-     */
     public String getValue() {
         return value;
     }
-
-    /**
-     * @param value the value to set
-     */
-    public void setValue(String value) {
-        this.value = value;
-    }
-
-    /**
-     * @return the fresh
-     */
-    public boolean isFresh() {
-        return fresh;
-    }
-    
 }
